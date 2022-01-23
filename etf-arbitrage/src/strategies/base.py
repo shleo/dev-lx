fd2b64d0a2bbdd65c4ba84227740ae18b32733f1
An Wu <anwu.andrew@hotmail.com>
2022-01-23 11:41:20 +0800

    Separate strategy into a package

---
+++ etf-arbitrage/src/strategies/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+import backtrader as bt
+
+
+class StrategyBase(bt.Strategy):
+    def __init__(self):
+        self.log('*' * 5 + 'BASE STRATEGY INITIALIZED')
+
+    def prenext(self):
+        self.log('*' * 5 + 'BEFORE MATURE')
+
+    def stop(self):
+        self.log('*' * 5 + 'BASE STRATEGY STOPPED')
+
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        print('[%s] %s' % (dt.isoformat(), txt))
---
+++ etf-arbitrage/src/strategies/simple_strategy.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import backtrader as bt
+
+from strategies.base import StrategyBase
+
+
+class SimpleStrategy(StrategyBase):
+    def __init__(self):
+        StrategyBase.__init__(self)
+
+        # Initialize member variables
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+        self.order = None
+        self.sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0], period=15)
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
+            else:  # Sell
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        self.order = None
+
+    def next(self):
+        StrategyBase.next(self)
+
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f : SMA, %.2f' % (self.dataclose[0], self.sma[0]))
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+
+            # Not yet ... we MIGHT BUY if ...
+            if self.dataclose[0] > self.sma[0]:
+
+                # BUY, BUY, BUY!!! (with all possible default parameters)
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.buy()
+
+        else:
+
+            if self.dataclose[0] < self.sma[0]:
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
