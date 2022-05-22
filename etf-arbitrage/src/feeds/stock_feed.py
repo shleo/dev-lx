fc61afdf68d17baaa62aeae03b63f7ad2388815f
An Wu <anwu.andrew@hotmail.com>
2022-05-22 09:31:11 +0800

    Add stock feed

---
+++ etf-arbitrage/src/feeds/stock_feed.py
@@ -0,0 +1,14 @@
+import backtrader.feeds as btfeed
+
+
+class StockFeed(btfeed.GenericCSVData):
+    params = (
+        ('dtformat', '%Y-%m-%d %H:%M:%S'),
+        ('datetime', 1),
+        ('open', 3),
+        ('high', 5),
+        ('low', 6),
+        ('close', 4),
+        ('volume', 8),
+        ('openinterest', -1)
+    )
